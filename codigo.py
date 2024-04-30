a4a5e987d7d4e1d319fb4ebad42e0f1c4a257360
Anthony <86032299+Anthony9933@users.noreply.github.com>
2024-04-30 15:44:58 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -48,6 +48,7 @@ def show_overview():
 def show_filters_data():
     st.header("Filtros e Dados")
     df = pd.read_csv('datatran2023.csv', encoding='latin-1', delimiter=';')
+    st.write('Gráficos')
     st.dataframe(df)
 
     Dia = st.sidebar.selectbox('Selecione o Dia', options=df['dia_semana'].unique())
@@ -60,7 +61,7 @@ def show_filters_data():
     fig = px.pie(contagem_id_por_uf, x='uf', y='id', labels={'id':'Quantidade de IDs', 'uf':'UF'},
                  title='Quantidade de IDs por UF')
     fig.show()
-    st.write(uf)
+    st.write(contagem_id_por_uf)
 
 # Página de Visão Geral
 if page == "Visão Geral":
