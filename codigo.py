a1b7dcc3b477f9b34c1998e81698ba709a956e2a
Anthony <86032299+Anthony9933@users.noreply.github.com>
2024-04-30 14:50:19 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -52,8 +52,6 @@ def show_filters_data():
     #df['dia_semana'] = pd.to_datetime(df['dia_semana']).dt.name
 
     Dia = st.sidebar.selectbox('Selecione o Dia', options=df['dia_semana'].unique())
-    early_access = st.sidebar.checkbox('Apenas Early Access')
-    recommendation = st.sidebar.checkbox('Apenas Recomendados')
 
     filtered_df = df[df['dia_semama'] == dia_semana]
 
