2380fd79a885b6ef29bd302600156c124d4e0539
Anthony <86032299+Anthony9933@users.noreply.github.com>
2024-04-30 16:45:54 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -74,7 +74,7 @@ def show_filters_data():
     soma_mortos_por_municipio.columns = ['Município', 'Quantidade de Mortos']
 
     # Criando o gráfico
-    fig = px.histogram(soma_mortos_por_municipio, x='Município', y='Quantidade de Acidentes','Quantidade de Mortos',
+    fig = px.histogram(soma_mortos_por_municipio, x='Município', y='Quantidade de Acidentes''Quantidade de Mortos',
                  title='Quantidade de Mortos por Município')
     #fig = px.bar(soma_mortos_por_municipio, x='Município', y='Quantidade de Mortos',
                  #title='Quantidade de Mortos por Município')
