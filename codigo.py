6519d96660461f24d686660401a10a312dc65829
Anthony <86032299+Anthony9933@users.noreply.github.com>
2024-05-20 11:49:19 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -19,9 +19,9 @@ def show_overview():
     st.write("O projeto utiliza um conjunto de dados com informações sobre as vendas e o estoque da loja. Aqui está uma breve visão "
              "geral dos principais componentes:")
     
-    st.markdown("- **Conjunto de Dados**: O conjunto de dados consiste em avaliações, cada uma contendo informações como data "
+    st.markdown("- *Conjunto de Dados*: O conjunto de dados consiste em avaliações, cada uma contendo informações como data "
                 "local, motivo, tipo e mais.")
-    st.markdown("- **Explicação das Colunas**: As colunas do conjunto de dados fornecem detalhes sobre vendas, produtos, "
+    st.markdown("- *Explicação das Colunas*: As colunas do conjunto de dados fornecem detalhes sobre vendas, produtos, "
                 "estoque e insights.")
     
     # Objetivo do Projeto
@@ -33,9 +33,9 @@ def show_overview():
     # Como Utilizar
     st.header("Como Utilizar")
     st.write("Para explorar o projeto, você pode navegar por diferentes seções usando a barra lateral. As principais seções incluem:")
-    st.markdown("- **Visualização do Conjunto de Dados**: Oferece uma visão rápida dos dados disponíveis.")
-    st.markdown("- **Descrição das Colunas**: Explica o significado de cada coluna no conjunto de dados.")
-    st.markdown("- **Estatísticas Resumidas**: Apresenta informações estatísticas sobre o conjunto de dados.")
+    st.markdown("- *Visualização do Conjunto de Dados*: Oferece uma visão rápida dos dados disponíveis.")
+    st.markdown("- *Descrição das Colunas*: Explica o significado de cada coluna no conjunto de dados.")
+    st.markdown("- *Estatísticas Resumidas*: Apresenta informações estatísticas sobre o conjunto de dados.")
     
     st.header("Conclusão")
     st.write("Sinta-se à vontade para analisar o conjunto de dados, obter insights e tirar conclusões significativas a partir "
@@ -118,4 +118,3 @@ elif page == "Filtros e Dados":
 
 #elif page == "Número de Acidentes por Hora do Dia":
     #show_accidents_by_hour()
-
